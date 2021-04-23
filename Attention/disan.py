a602eff2337fab39b3678f8c3a78c2ffb7f53173
fadelmuli <72653111+fadelmuli@users.noreply.github.com>
2021-04-23 13:06:59 +0700

    Update disan.py

--- Attention/disan.py
+++ Attention/disan.py
@@ -7,8 +7,6 @@ Directional Self-Attention Network
 Compatibility: Python 3.5.2, Tensorflow 1.2
 """
 
-%tensorflow_version 1.x
-
 import tensorflow as tf
 from functools import reduce
 from operator import mul
